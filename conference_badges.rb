d74b42d0a4b9b77c4b0374f1f88c6bbbcd44d6a8
Thor Summerall <thor.summerall@gmail.com>
2020-11-17 19:19:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  list_of_badges = "Hello, my name is #{attendees}"
+  list_of_badges = badge_maker(name)
   return list_of_badges
 end
 
