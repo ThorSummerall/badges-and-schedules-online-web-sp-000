2df69231665acb6d90c74ca3fbc91e0a2dc80d7b
Thor Summerall <thor.summerall@gmail.com>
2020-11-17 19:23:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ def assign_rooms(speakers)
   assigned_rooms = []
 
   speakers.each_with_index do |name, index|
-    assigned_rooms << "Hello, #{name}! You'll be assigned to room #{index}!"
+    assigned_rooms << "Hello, #{name}! You'll be assigned to room #{index+1}!"
   end
   assigned_rooms
 end
